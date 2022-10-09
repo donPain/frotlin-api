a5d6f9c975f06b07e2276730841ef2629b97cb51
Eduardo <eduardo.plis@hotmail.com>
2022-10-09 14:11:20 -0300

    first commit

---
+++ src/main/kotlin/com/embarcshit/fckdelphi/FckdelphiApplication.kt
@@ -0,0 +1,11 @@
+package com.embarcshit.fckdelphi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FckdelphiApplication
+
+fun main(args: Array<String>) {
+	runApplication<FckdelphiApplication>(*args)
+}
---
+++ src/test/kotlin/com/embarcshit/fckdelphi/FckdelphiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.embarcshit.fckdelphi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FckdelphiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
